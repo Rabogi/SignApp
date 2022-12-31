0c58661a461557d6d5391c6120c9e3c665374b5a
Rabogi <xkrules2002@mail.ru>
2022-12-31 09:15:25 +0300

    file hasher is now returning correct results

--- Shared/HashMaster.cs
+++ Shared/HashMaster.cs
@@ -8,7 +8,7 @@ namespace SignApp.Shared
 
     public abstract class HashMaster
     {
-        public static string Hash256(string input)
+        public static string Hash256Str(string input)
         {
             var hasher = SHA256.Create();
             Encoding enc = Encoding.UTF8;
@@ -20,6 +20,17 @@ namespace SignApp.Shared
             }
             return hash.ToString();
         }
-
+        
+        public static string Hash256File(byte[] input)
+        {   
+            var hasher = SHA256.Create();
+            byte[] output = hasher.ComputeHash(input);
+            var hash = new StringBuilder();
+            foreach (byte b in output)
+            {
+                hash.Append(b.ToString("x2"));
+            }
+            return hash.ToString();
+        }
     }
 }
